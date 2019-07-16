4ae5be403103aa109cc4acca3d1cb02c67d8f9bb
ibrahimalausa <ibrahim.alausa@andela.com>
2019-07-16 15:26:18 +0100

    Updates

---
+++ precache-manifest.a5161e82658286787e24e50dbba7c86a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13d256dd50ec81741d44d842e6877ce7",
+    "url": "/RandomPeople/index.html"
+  },
+  {
+    "revision": "f5bbef69bdc740c13e50",
+    "url": "/RandomPeople/static/css/main.9034cb2a.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/RandomPeople/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "f5bbef69bdc740c13e50",
+    "url": "/RandomPeople/static/js/main.8477a617.chunk.js"
+  },
+  {
+    "revision": "d444218897e58130d54b",
+    "url": "/RandomPeople/static/js/runtime~main.b966a281.js"
+  }
+]);
